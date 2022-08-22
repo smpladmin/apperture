3a8cb9164e19151010926ffcc20d51ac63de083f
Aman <aman@parallelhq.com>
2022-08-22 14:17:04 +0530

    adding sidebar and topbar

---
+++ frontend/components/Searchbar.tsx
@@ -0,0 +1,32 @@
+import styles from "../components/Searchbar.module.css";
+import "remixicon/fonts/remixicon.css";
+import filter_icon from "../assets/icons/filter-icon.svg";
+import mixPanel from "../assets/images/mixPanel-icon.png";
+
+const Searchbar = () => {
+  return (
+    <div className={styles.searchBar}>
+      <div className={styles.searchBar__wrapper}>
+        <div className={styles.searchBar__left}>
+          <input
+            placeholder="Search for events"
+            className={styles.searchBar__input}
+          />
+        </div>
+        <div className={styles.searchBar__right}>
+          <div className={styles.searchBar__dateRange}>
+            <i className="ri-calendar-fill"></i>
+          </div>
+          <div className={styles.searchBar__filterButton}>
+            <img src={filter_icon} alt="filter-icon" />
+          </div>
+          <div className={styles.searchBar__dataSource}>
+            {/* <img src={mixPanel} alt="data-source-mix-panel" /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Searchbar;
--- frontend/pages/login/index.tsx
+++ frontend/pages/login/index.tsx
@@ -1,9 +1,14 @@
-import styles from './Login.module.css';
+import styles from "./Login.module.css";
+import Sidebar from "../../components/Sidebar";
+import Searchbar from "../../components/Searchbar";
 
 const Login = () => {
-  return <div className={styles.loginContainer}>
-    Login page
-  </div>
-}
+  return (
+    <div className={styles.login}>
+      <Sidebar />
+      <Searchbar />
+    </div>
+  );
+};
 
 export default Login;
