9707adaa66838f8e09be42f8c0baa73801057e53
Roopak Nijhara <roopak@parallelhq.com>
2022-08-24 18:17:03 +0530

    Copy code to /code directory when building image

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -3,8 +3,10 @@ import type { NextRequest } from 'next/server'
 import { validateToken } from './lib/services/authService'
 
 export async function middleware(request: NextRequest) {
+  console.log("-------------middleware -----------", request.url);
   const userId = await validateToken(request.cookies.get("auth_token"));
   if (!userId) {
+    console.log("---------redirecting to login page--------")
     return NextResponse.redirect(new URL("/login", request.url));
   }
 }
