454a16b7bebe62fff5c07ec3e6e9d1953d114ef0
Roopak Nijhara <roopak@parallelhq.com>
2022-08-25 10:54:52 +0530

    Add chakra ui

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,16 @@
 import "../styles/globals.css";
 import { AppLayoutProps } from 'next/app';
 import { ReactNode } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppLayoutProps) {
   const getLayout = Component.getLayout || ((page: ReactNode) => page)
 
-  return getLayout(<Component {...pageProps} />)
+  return (
+    <ChakraProvider>
+      {getLayout(<Component {...pageProps} />)}
+    </ChakraProvider>
+  )
 }
 
 export default MyApp;
