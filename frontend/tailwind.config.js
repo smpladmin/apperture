e60ee01a5dfa96446d83bfafffb4b9ae13ca32c7
Roopak Nijhara <roopak@parallelhq.com>
2022-08-25 11:46:52 +0530

    Add colors to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
+    colors: {
+      black: {
+        DEFAULT: '#07070d',
+        100: '#0e0e1a'
+      },
+      white: '#ffffff',
+      grey: '#d9d9da',
+      yellow: '#fac213',
+      green: '#57aa64',
+      'hover-grey': '#78787e'
+    },
     extend: {},
   },
   plugins: [],
