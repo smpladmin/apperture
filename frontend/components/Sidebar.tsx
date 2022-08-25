812699bb468c053745c32032d74ae7e1a48824d8
Aman <aman@parallelhq.com>
2022-08-25 14:20:21 +0530

    sidebar-tailwind-refactoring

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -1,37 +1,38 @@
-import styles from "../components/Sidebar.module.css";
 import "remixicon/fonts/remixicon.css";
 import logo from "../assets/images/apperture_white-icon.svg";
 import Link from "next/link";
 
 const Sidebar = () => {
   return (
-    <div className={styles.sideBar}>
-      <img src={logo.src} className={styles.sideBar_logo} alt="appertureLogo" />
-      <div className={styles.sideBar__app}>
-        <p className={styles.sideBar__headings}>APP</p>
-        <div className={styles.sideBar__accountImage}>
+    <div className="w-full h-screen	max-w-{64px} pt-3 pb-12 flex flex-col items-center text-xs bg-aptBlack text-white text-center	max-w-4">
+      <img src={logo.src} className="pb-10" alt="appertureLogo" />
+      <div>
+        <p className="opacity-30 text-xs-10">APP</p>
+        <div className="w-8 h-8 flex items-center justify-center bg-yellow-400 rounded-full	text-xs-14 font-bold mt-4 mb-10">
           <p>ZA</p>
         </div>
       </div>
-      <div className={styles.sideBar__explore}>
-        <p className={styles.sideBar__headings}>EXPLORE</p>
-        <div className={styles.sideBar_buttonWrapper}>
-          <div className={styles.sideBar__buttons}>
-            <div className={styles.rotate45}>
+      <div>
+        <p className="opacity-30 text-xs-10">EXPLORE</p>
+        <div className="pt-5 flex flex-col items-center gap-y-5 ">
+          <div className="w-10 h-10 grid place-items-center rounded-lg cursor-pointer	transition ease-in duration-250 bg-white bg-opacity-0	 hover:bg-opacity-10 ">
+            <div className="transform rotate-45">
               <i className="ri-route-fill"></i>
             </div>
           </div>
-          <div className={styles.sideBar__buttons}>
+          <div className="w-10 h-10 grid place-items-center rounded-lg cursor-pointer	transition ease-in duration-250 bg-white bg-opacity-0	 hover:bg-opacity-10 ">
             <i className="ri-lightbulb-line"></i>
           </div>
-          <div className={styles.sideBar__buttons}>
+          <div className="w-10 h-10 grid place-items-center rounded-lg cursor-not-allowed	transition ease-in duration-250 bg-white bg-opacity-0	 hover:bg-opacity-10 ">
             <i className="ri-bookmark-line"></i>
           </div>
-          <div className={styles.sideBar__tag}>Coming soon</div>
+          <div className="-mt-4 bg-aptGreen p-1 font-medium rounded-sm text-xs-8">
+            Coming soon
+          </div>
         </div>
       </div>
-      <div className={styles.sideBar__logOut}>
-        <div className={styles.sideBar__buttons}>
+      <div className="mt-auto w-10 h-10 grid place-items-center rounded-lg cursor-pointer	transition ease-in duration-250 bg-white bg-opacity-0	 hover:bg-opacity-10 ">
+        <div>
           <Link href={`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/logout`}>
             <i className="ri-logout-box-r-line"></i>
           </Link>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,10 +2,24 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      aptBlack: "#0E0E1A",
+      aptGreen: "#57AA64",
+    }),
+
+    maxWidth: {
+      4: "4rem",
+    },
+
+    fontSize: {
+      "xs-8": ["0.5rem", "1.5"],
+      "xs-10": ["0.625rem", "1.2"],
+      "xs-14": ["0.875rem", "1.3"],
+    },
   },
   plugins: [],
-}
+};
