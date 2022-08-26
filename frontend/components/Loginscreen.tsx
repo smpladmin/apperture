fd88efed1c8166fba4cc4f1507ac35764cd47df1
Roopak Nijhara <roopak@parallelhq.com>
2022-08-26 08:25:49 +0530

    Remove Loginscreen module css

--- frontend/components/Loginscreen.tsx
+++ frontend/components/Loginscreen.tsx
@@ -1,4 +1,3 @@
-import styles from '../components/Loginscreen.module.css';
 import Image from 'next/image';
 import logo from '../assets/images/Logo_login.svg';
 import glogo from '../assets/images/Google_login.svg';
@@ -10,7 +9,7 @@ const Loginscreen = () => {
       <div className="flex h-full flex-col justify-between md:mx-36 lg:ml-45">
         <div className="mt-25 md:mt-32">
           <div className={'relative mb-3 h-25 w-25 md:h-30 md:w-30'}>
-            <Image src={logo} layout="fill" />
+            <Image src={logo} layout="fill" alt="Apperture logo" />
           </div>
           <h1 className="mt-12 text-[34px] font-normal leading-[42px] md:text-[56px] md:leading-[65px]">
             Product Analytics <br /> for everyone
@@ -22,7 +21,7 @@ const Loginscreen = () => {
         <div className="mt-24 mb-2 flex flex-col items-center gap-6 md:max-w-[565px]">
           <Link href={`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/login`}>
             <div className="flex w-full cursor-pointer items-center justify-center gap-6 rounded-lg bg-white p-6 text-[20px] font-semibold leading-[22px] text-black-100">
-              <Image src={glogo} />
+              <Image src={glogo} alt="Google logo" />
               Sign up with Google
             </div>
           </Link>
