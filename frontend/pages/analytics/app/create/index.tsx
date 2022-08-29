4ae34dcd75c3d6be2344ef0fab2381c51f22e7ee
Aman <aman@parallelhq.com>
2022-08-29 08:20:09 +0530

    mobile responsive screen

--- frontend/pages/analytics/app/create/index.tsx
+++ frontend/pages/analytics/app/create/index.tsx
@@ -14,40 +14,48 @@ const Create = () => {
   const handleGoBack = (): void => router.back();
 
   return (
-    <div className="px-48 pt-20 lg:max-w-screen-xl">
-      <div
-        tabIndex={0}
-        className="mb-11 flex h-9 w-9 items-center justify-center rounded-full border"
-        onClick={handleGoBack}
-      >
-        <i className="ri-close-fill"></i>
-      </div>
-      <div className="max-w-3xl">
-        <p className="text-grey-200 pb-6 text-xs-14 font-medium">Step 1 of 3</p>
-        <h2 className="pb-10 text-[3.5rem] font-semibold leading-[4.125rem]">
-          What would you like to name this application?
-        </h2>
-        <Input
-          size={'lg'}
-          w={100}
-          bg={'white.200'}
-          rounded={'0.25rem'}
-          fontSize={'md'}
-          placeholder="Ex- Food Web App"
-          className="py-4 px-3.5 text-base text-black"
-          _placeholder={{
-            fontSize: '1rem',
-            lineHeight: '1.375rem',
-            fontWeight: 400,
-            color: 'grey.100',
-          }}
-          value={appName}
-          onChange={(e) => setAppName(e.target.value)}
-        />
+    <div className="flex h-screen flex-col justify-between p-4 lg:h-auto lg:max-w-screen-xl lg:px-48 lg:pt-20">
+      <div>
+        <div
+          tabIndex={0}
+          className="mb-11 flex h-9 w-9 items-center justify-center rounded-full border"
+          onClick={handleGoBack}
+        >
+          <i className="ri-close-fill"></i>
+        </div>
+        <div className="sm:w-full lg:max-w-3xl">
+          <p className="text-grey-200 pb-6 text-xs-14 font-medium">
+            Step 1 of 3
+          </p>
+          <h2 className="pb-10 text-[3.5rem] font-semibold leading-[4.125rem]">
+            What would you like to name this application?
+          </h2>
+          <Input
+            size={'lg'}
+            width={[
+              '100%', // base
+              '100%', // 480px upwards
+              '500px', // 768px upwards
+            ]}
+            bg={'white.200'}
+            rounded={'0.25rem'}
+            fontSize={'md'}
+            placeholder="Ex- Food Web App"
+            className="py-4 px-3.5 text-base text-black"
+            _placeholder={{
+              fontSize: '1rem',
+              lineHeight: '1.375rem',
+              fontWeight: 400,
+              color: 'grey.100',
+            }}
+            value={appName}
+            onChange={(e) => setAppName(e.target.value)}
+          />
+        </div>
       </div>
       {/* to decide whther to wrap in form or wrap inside div to make key board accessible*/}
       <button
-        className="mt-10 flex w-72 items-center justify-center rounded-lg bg-black-100 p-4 text-base font-semibold text-white-100 disabled:pointer-events-none disabled:bg-grey"
+        className="mt-10 flex items-center justify-center rounded-lg bg-black-100 p-4 text-base font-semibold text-white-100 disabled:pointer-events-none disabled:bg-grey sm:w-full lg:w-72 lg:w-auto"
         disabled={!appName}
         onClick={handleNextClick}
       >
