fcb5f3d704a98f9ed1879e99ca1189e9f6abfe5c
Roopak Nijhara <roopak@parallelhq.com>
2022-09-05 10:26:46 +0530

    Add query param for apps modal

--- frontend/components/Layout/index.tsx
+++ frontend/components/Layout/index.tsx
@@ -4,28 +4,44 @@ import Header from '../Header';
 import Sidebar from '../Sidebar';
 import { App } from '@lib/domain/app';
 import AppsModal from '@components/Sidebar/AppsModal';
+import { useRouter } from 'next/router';
 
 export default function Layout({ children, apps = [] }: LayoutProps) {
+  const router = useRouter();
   const [selectedAppId, setSelectedAppId] = useState(apps[0]._id);
   const [selectedApp, setSelectedApp] = useState(apps[0]);
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure({
+    defaultIsOpen: !!router.query.apps,
+  });
 
   useEffect(() => {
     setSelectedApp(apps.find((a) => a._id === selectedAppId)!!);
   }, [apps, selectedAppId]);
 
+  const onModalOpen = () => {
+    onOpen();
+    router.replace({ query: { ...router.query, apps: 1 } });
+  };
+
+  const onModalClose = () => {
+    onClose();
+    const query = router.query;
+    delete query.apps;
+    router.replace({ query: { ...query } });
+  };
+
   return (
     <Flex flexDir={'row'}>
       <AppsModal
         isOpen={isOpen}
-        onClose={onClose}
+        onClose={onModalClose}
         apps={apps}
         selectApp={setSelectedAppId}
         selectedApp={selectedApp}
       />
-      <Sidebar selectedApp={selectedApp} openAppsModal={onOpen} />
+      <Sidebar selectedApp={selectedApp} openAppsModal={onModalOpen} />
       <Flex flexDir={'column'} w={'full'}>
-        <Header selectedApp={selectedApp} openAppsModal={onOpen} />
+        <Header selectedApp={selectedApp} openAppsModal={onModalOpen} />
         <main>{children}</main>
       </Flex>
     </Flex>
