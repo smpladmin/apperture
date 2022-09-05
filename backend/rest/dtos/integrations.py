faaa81bd262d53a792cd962d41b2d30f32aaedc5
Roopak Nijhara <roopak@parallelhq.com>
2022-09-05 15:53:15 +0530

    Add endpoint to create integration with datasource

--- backend/rest/dtos/integrations.py
+++ backend/rest/dtos/integrations.py
@@ -1,7 +1,23 @@
 from typing import Optional
+
+from pydantic import BaseModel
+from domain.common.models import IntegrationProvider
 from domain.integrations.models import Credential, Integration
+from rest.dtos.datasources import DataSourceResponse
 from .model_response import ModelResponse
 
 
 class IntegrationResponse(Integration, ModelResponse):
     credential: Optional[Credential]
+    datasource: Optional[DataSourceResponse]
+
+    class Config:
+        orm_mode = True
+
+
+class CreateIntegrationDto(BaseModel):
+    appId: str
+    provider: IntegrationProvider
+    accountId: str
+    apiKey: str
+    apiSecret: str
