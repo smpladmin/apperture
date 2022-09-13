e5c702400f59489ede32dc6fe9ba4956fe2fdcb5
Roopak Nijhara <roopak@parallelhq.com>
2022-09-13 13:10:38 +0530

    Add ability to render network graph using GA edge data

--- frontend/lib/types/graph.ts
+++ frontend/lib/types/graph.ts
@@ -1,17 +1,15 @@
 export type ApiDataType = {
-  previousPage: string;
-  pagePath: string;
+  previousEvent: string;
+  currentEvent: string;
   users: number;
-  id: number | string;
-  createdAt: number;
-  viewId: string;
+  _id: number | string;
 };
 
 export type EdgeType = {
   source: string;
   target: string;
-  previousPage: string;
-  pagePath: string;
+  previousEvent: string;
+  currentEvent: string;
   type: string;
   node1Label: number;
   traffic: number;
@@ -26,7 +24,7 @@ export type NodeType = {
   label: string;
   type: string;
   isHidden?: boolean;
-  pageName: string;
+  eventName: string;
   percentile?: number;
   visibleAt?: number;
   totalViews?: number;
