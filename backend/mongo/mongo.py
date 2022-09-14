84f4e30c60e20771c376d804a4f380fbc1b3833d
rnaidu <rahul@parallelhq.com>
2022-09-14 16:12:12 +0530

    Initialized new collection

--- backend/mongo/mongo.py
+++ backend/mongo/mongo.py
@@ -6,6 +6,7 @@ from domain.datasources.models import DataSource
 from domain.edge.models import Edge
 from domain.integrations.models import Integration
 from domain.users.models import User
+from domain.ga_cleaned_data.models import GACleanedData
 
 
 class Mongo:
@@ -13,7 +14,7 @@ class Mongo:
         self.client = motor.motor_asyncio.AsyncIOMotorClient(os.environ.get("DB_URI"))
         await init_beanie(
             database=self.client[os.environ.get("DB_NAME")],
-            document_models=[User, App, Integration, DataSource, Edge],
+            document_models=[User, App, Integration, DataSource, Edge, GACleanedData],
         )
 
     async def close(self):
