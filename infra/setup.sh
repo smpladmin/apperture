a7849e7b2cb94adabc4ca0b94d4859461bfc4e2c
Roopak Nijhara <roopak@parallelhq.com>
2022-09-23 16:53:35 +0530

    Adds ability to setup staging and production

---
+++ infra/setup.sh
@@ -0,0 +1,7 @@
+export USE_HOSTNAME=$1
+echo $USE_HOSTNAME > /etc/hostname
+hostname -F /etc/hostname
+apt-get update
+curl -fsSL get.docker.com -o get-docker.sh
+CHANNEL=stable sh get-docker.sh
+rm get-docker.sh
--- infra/src/expected.py
+++ infra/src/expected.py
@@ -1,5 +1,6 @@
 import pulumi
 import pulumi_aws as aws
+from pulumi import export
 
 config = pulumi.Config()
 data = config.require_object("data")
@@ -12,6 +13,38 @@ igw = aws.ec2.InternetGateway(
     tags={
         "Name": data.get("igw_name"),
     },
+)
+
+privatesubnet = aws.ec2.Subnet(
+    data.get("prv_subnet_name"),
+    vpc_id=virtualprivatecloud.id,
+    cidr_block=data.get("prv_cidr"),
+    map_public_ip_on_launch=False,
+    tags={
+        "Name": data.get("prv_subnet_name"),
+    },
+)
+
+publicsubnet = aws.ec2.Subnet(
+    data.get("pub_subnet_name"),
+    vpc_id=virtualprivatecloud.id,
+    cidr_block=data.get("pub_cidr"),
+    map_public_ip_on_launch=True,
+    tags={
+        "Name": data.get("pub_subnet_name"),
+    },
+)
+
+eip = aws.ec2.Eip(data.get("eip_name"), vpc=True)
+
+natgateway = aws.ec2.NatGateway(
+    data.get("natgw_name"),
+    allocation_id=eip.allocation_id,
+    subnet_id=publicsubnet.id,
+    tags={
+        "Name": data.get("natgw_name"),
+    },
+    opts=pulumi.ResourceOptions(depends_on=[igw]),
 )
 
 pubroutetable = aws.ec2.RouteTable(
@@ -28,13 +61,17 @@ pubroutetable = aws.ec2.RouteTable(
     },
 )
 
-publicsubnet = aws.ec2.Subnet(
-    data.get("pub_subnet_name"),
+prvroutetable = aws.ec2.RouteTable(
+    data.get("prvrttable_name"),
     vpc_id=virtualprivatecloud.id,
-    cidr_block=data.get("pub_cidr"),
-    map_public_ip_on_launch=True,
+    routes=[
+        aws.ec2.RouteTableRouteArgs(
+            cidr_block="0.0.0.0/0",
+            gateway_id=natgateway.id,
+        )
+    ],
     tags={
-        "Name": data.get("pub_subnet_name"),
+        "Name": data.get("prvrttable_name"),
     },
 )
 
@@ -42,6 +79,12 @@ pub_route_association = aws.ec2.RouteTableAssociation(
     data.get("pubrtasst_name"),
     route_table_id=pubroutetable.id,
     subnet_id=publicsubnet.id,
+)
+
+prv_route_association = aws.ec2.RouteTableAssociation(
+    data.get("prvrtasst_name"),
+    route_table_id=prvroutetable.id,
+    subnet_id=privatesubnet.id,
 )
 
 sg = aws.ec2.SecurityGroup(
@@ -70,31 +113,31 @@ sg = aws.ec2.SecurityGroup(
             "protocol": "tcp",
             "from_port": 2376,
             "to_port": 2376,
-            "cidr_blocks": [data.get("pub_cidr")],
+            "cidr_blocks": [data.get("parent_cidr")],
         },
         {
             "protocol": "tcp",
             "from_port": 2377,
             "to_port": 2377,
-            "cidr_blocks": [data.get("pub_cidr")],
+            "cidr_blocks": [data.get("parent_cidr")],
         },
         {
             "protocol": "tcp",
             "from_port": 7946,
             "to_port": 7946,
-            "cidr_blocks": [data.get("pub_cidr")],
+            "cidr_blocks": [data.get("parent_cidr")],
         },
         {
             "protocol": "udp",
             "from_port": 7946,
             "to_port": 7946,
-            "cidr_blocks": [data.get("pub_cidr")],
+            "cidr_blocks": [data.get("parent_cidr")],
         },
         {
             "protocol": "tcp",
             "from_port": 4789,
             "to_port": 4789,
-            "cidr_blocks": [data.get("pub_cidr")],
+            "cidr_blocks": [data.get("parent_cidr")],
         },
     ],
     egress=[
@@ -108,13 +151,7 @@ sg = aws.ec2.SecurityGroup(
     vpc_id=virtualprivatecloud.id,
 )
 
-keypair = aws.ec2.KeyPair(
-    data.get("key_name"),
-    public_key=data.get("public_key"),
-    tags={
-        "Name": data.get("key_name"),
-    },
-)
+keypair = aws.ec2.KeyPair("apperture-key", public_key=data.get("public_key"))
 
 ami = aws.ec2.get_ami(
     most_recent=True,
@@ -129,24 +166,14 @@ public_ec2_instance = aws.ec2.Instance(
     ami=ami.id,
     key_name=keypair.key_name,
     subnet_id=publicsubnet.id,
-    tags={
-        "Name": data.get("ec2_public_name"),
-        "Swarm": "manager",
-    },
 )
 
-eip = aws.ec2.Eip(
-    data.get("eip_name"),
-    vpc=True,
-    tags={
-        "Name": data.get("eip_name"),
-    },
-)
+dev_eip = aws.ec2.Eip("apperture-dev-ip", vpc=True)
 
 eip_assoc = aws.ec2.EipAssociation(
-    "apperture-eip-assoc",
+    "apperture-dev-eip-assoc",
     instance_id=public_ec2_instance.id,
-    allocation_id=eip.id,
+    allocation_id=dev_eip.id,
 )
 
 for i in range(1, data.get("ec2_private_count") + 1):
@@ -156,10 +183,5 @@ for i in range(1, data.get("ec2_private_count") + 1):
         vpc_security_group_ids=[sg.id],
         ami=ami.id,
         key_name=keypair.key_name,
-        subnet_id=publicsubnet.id,
-        associate_public_ip_address=True,
-        tags={
-            "Name": f"{data.get('ec2_private_name')}-{i}",
-            "Swarm": f"worker-{i}",
-        },
+        subnet_id=privatesubnet.id,
     )
