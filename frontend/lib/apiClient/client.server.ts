f2c4ad192fe454d1b60e90fa6e1566998f168de0
Roopak Nijhara <roopak@parallelhq.com>
2022-09-27 12:04:18 +0530

    Use runtime config in frontend

--- frontend/lib/apiClient/client.server.ts
+++ frontend/lib/apiClient/client.server.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
+import getConfig from 'next/config';
+const { serverRuntimeConfig: config } = getConfig();
 
 export const ApperturePrivateAPI = axios.create({
-  baseURL: process.env.BACKEND_BASE_URL,
+  baseURL: config.BACKEND_BASE_URL,
 });
--- frontend/userAnalytics.tsx
+++ frontend/userAnalytics.tsx
@@ -2,13 +2,15 @@ import Analytics from 'analytics';
 // @ts-ignore
 import amplitudePlugin from '@analytics/amplitude';
 import Router from 'next/router';
+import getConfig from 'next/config';
+const { publicRuntimeConfig: config } = getConfig();
 
 const plugins =
   process.env.NODE_ENV === 'development'
     ? []
     : [
         amplitudePlugin({
-          apiKey: process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY,
+          apiKey: config.PUBLIC_AMPLITUDE_API_KEY,
           options: {},
         }),
       ];
