b1f4ac4f97a4715f0659bf0c68340b3da4d92300
rnaidu <rahul@parallelhq.com>
2022-09-28 13:55:53 +0530

    Added date filter in query params for apis

--- backend/rest/controllers/datasource_controller.py
+++ backend/rest/controllers/datasource_controller.py
@@ -20,15 +20,21 @@ async def get_edges(ds_id: str, start_date: str = '1970-01-01', end_date: str = 
 
 
 @router.get("/datasources/{ds_id}/trends", response_model=list[NodeTrendResponse])
-async def get_trend_nodes(ds_id: str, node: str, trend_type: str, edge_service: EdgeService = Depends()):
-    return await edge_service.get_node_trends(ds_id, node, trend_type)
+async def get_trend_nodes(ds_id: str, node: str, trend_type: str,
+                          start_date: str = '1970-01-01', end_date: str = dt.today().strftime('%Y-%m-%d'),
+                          edge_service: EdgeService = Depends()):
+    return await edge_service.get_node_trends(ds_id, node, trend_type, start_date, end_date)
 
 
 @router.get("/datasources/{ds_id}/sankey", response_model=list[NodeSankeyResponse])
-async def get_sankey_nodes(ds_id: str, node: str, edge_service: EdgeService = Depends()):
-    return await edge_service.get_node_sankey(ds_id, node)
+async def get_sankey_nodes(ds_id: str, node: str,
+                           start_date: str = '1970-01-01', end_date: str = dt.today().strftime('%Y-%m-%d'),
+                           edge_service: EdgeService = Depends()):
+    return await edge_service.get_node_sankey(ds_id, node, start_date, end_date)
 
 
 @router.get("/datasources/{ds_id}/node_significance", response_model=list[NodeSignificanceResponse])
-async def get_sankey_nodes(ds_id: str, node: str, edge_service: EdgeService = Depends()):
-    return await edge_service.get_node_significance(ds_id, node)
+async def get_sankey_nodes(ds_id: str, node: str,
+                           start_date: str = '1970-01-01', end_date: str = dt.today().strftime('%Y-%m-%d'),
+                           edge_service: EdgeService = Depends()):
+    return await edge_service.get_node_significance(ds_id, node, start_date, end_date)
