7fb6483e9f993da2f21cfc059132296d88ddce38
anish-parallel <anish@parallelhq.com>
2022-10-06 13:34:12 +0530

    refactor wheelzoom issue

--- frontend/components/Graph/index.tsx
+++ frontend/components/Graph/index.tsx
@@ -143,6 +143,42 @@ const Graph = ({ visualisationData }: GraphProps) => {
   }, [activeNode]);
 
   useEffect(() => {
+    G6.registerBehavior('wheel-zoom', {
+      getEvents() {
+        return {
+          wheelzoom: 'onWheelZoom',
+        };
+      },
+      onWheelZoom() {
+        const graph = gRef.current.graph;
+        const zoomRatio = graph?.getZoom()!!;
+        const nodes = graph?.getNodes()!!;
+        const edges = graph?.getEdges();
+
+        const activatedNodes = graph?.findAllByState('node', 'active');
+
+        if (zoomRatio >= graphConfig.minZoom) {
+          showAndHideNodesOnZoom(graph, nodes, zoomRatio);
+        }
+        if (activatedNodes?.length) {
+          graph?.updateItem(activatedNodes[0], {
+            stateStyles: {
+              active: {
+                stroke: '#000000',
+                fill: '#ffffff',
+                lineWidth: 6 / zoomRatio!!,
+                shadowColor: '#ffffff',
+                shadowBlur: 6,
+              },
+            },
+          });
+        }
+        setNodesAndEdgesStyleOnZoom(nodes!!, edges!!, zoomRatio!!);
+      },
+    });
+  }, []);
+
+  useEffect(() => {
     if (!gRef.current.graph) {
       G6.registerNode(
         'primary',
@@ -207,21 +243,6 @@ const Graph = ({ visualisationData }: GraphProps) => {
         'line'
       );
 
-      G6.registerBehavior('wheel-zoom', {
-        getEvents() {
-          return {
-            wheelzoom: 'onWheelZoom',
-          };
-        },
-        onWheelZoom() {
-          const graph = gRef.current.graph;
-          const zoomRatio = graph?.getZoom();
-          const nodes = graph?.getNodes();
-          const edges = graph?.getEdges();
-          setNodesAndEdgesStyleOnZoom(nodes!!, edges!!, zoomRatio!!);
-        },
-      });
-
       gRef.current.graph = new G6Graph({
         container: ref.current || '',
         width: ref.current?.offsetWidth,
@@ -258,14 +279,6 @@ const Graph = ({ visualisationData }: GraphProps) => {
       const nodes = graph.getNodes();
       const zoomRatio = graph.getZoom();
       showAndHideNodesOnZoom(graph, nodes, zoomRatio);
-    });
-
-    graph.on('wheelzoom', () => {
-      const nodes = graph.getNodes();
-      const zoomRatio = graph.getZoom();
-      if (zoomRatio >= graphConfig.minZoom) {
-        showAndHideNodesOnZoom(graph, nodes, zoomRatio);
-      }
     });
 
     graph.on('dragnodeend', (evt: IG6GraphEvent) => {
