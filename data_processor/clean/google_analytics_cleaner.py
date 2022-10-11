a15a4ce16b1f03ed68adf4b820128b0f6874ed5a
Aditya Priyam <apriyam9@gmail.com>
2022-10-11 15:39:40 +0530

    Cleaner impementation with regex

--- data_processor/clean/google_analytics_cleaner.py
+++ data_processor/clean/google_analytics_cleaner.py
@@ -1,7 +1,7 @@
 import re
 import logging
-import pandas as pd
-from urllib.parse import urlparse
+import pandas as pd 
+
 
 from .cleaner import Cleaner
 
@@ -15,12 +15,36 @@ class GoogleAnalyticsCleaner(Cleaner):
         logging.info("{x}: {y}".format(x="Length of df after cleanup", y=len(df)))
         return df
 
+    def remove_locale_from_endpoint(self, url:str)->str:
+        endpoint = re.sub("/[a-zA-Z]{2,2}(-[a-zA-Z]{2})?/","/",url)
+        # Regext to test locale at end
+        endpoint = re.sub("/[a-zA-Z]{2,2}(-[a-zA-Z]{2})?$","/",endpoint) 
+        if url != endpoint:
+            logging.info("{x}: {y}".format(x="Removed locale for {0}".format(url), y=endpoint))
+        return endpoint
+    
+    def convert_url_to_endpoint(self, url:str)->str:
+        endpoint = re.search("(?P<domain>.+(\.\w{2,})+)(?P<endpoint>/.*)",url)
+        if endpoint: 
+            endpoint=endpoint.group("endpoint") 
+            logging.info("{x}: {y}".format(x="Converted {0} to".format(url), y=endpoint))
+            return endpoint
+        else:
+            logging.info("{x}: {y}".format(x="Could not convert", y=url))
+            return url
+
     def convert_url_to_path(self, df):
         df["previousPage"] = df["previousPage"].apply(
-            lambda x: urlparse(x).path
+            self.convert_url_to_endpoint
+        )
+        df["previousPage"] = df["previousPage"].apply(
+            self.remove_locale_from_endpoint
         )
         df["pagePath"] = df["pagePath"].apply(
-            lambda x: urlparse(x).path
+            self.convert_url_to_endpoint
+        )
+        df["pagePath"] = df["pagePath"].apply(
+            self.remove_locale_from_endpoint
         )
 
     def convert_data_types(self, df):
