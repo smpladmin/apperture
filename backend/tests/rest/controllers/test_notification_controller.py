4bc96e9074c83a35a34e61d8f1cbfac8a3e478dc
Aditya Priyam <apriyam9@gmail.com>
2022-11-01 10:32:28 +0530

    Changes in tests build process for backend, fixed malicios assertion in notification test file

--- backend/tests/rest/controllers/test_notification_controller.py
+++ backend/tests/rest/controllers/test_notification_controller.py
@@ -6,5 +6,5 @@ def test_get_notification(app_init,notification_response):
         test_client= TestClient(app_init)
         response = test_client.get("/notifications/?name=name") 
         assert response.status_code == 200
-        assert response.json().keys() != notification_response.keys()
+        assert response.json().keys() == notification_response.keys()
 
