6fb5ab91c7a924d1d00d93de3281d5d63fc87554
rnaidu <rahul@parallelhq.com>
2022-11-02 11:52:57 +0530

    reformatting

--- backend/tests/rest/controllers/test_notification_controller.py
+++ backend/tests/rest/controllers/test_notification_controller.py
@@ -21,7 +21,9 @@ def test_add_notification(client_init, notification_data, notification_response)
 
 
 def test_update_notification(client_init, notification_data, notification_response):
-    response = client_init.put("/notifications/635ba034807ab86d8a2aadd8", data=json.dumps(notification_data))
+    response = client_init.put(
+        "/notifications/635ba034807ab86d8a2aadd8", data=json.dumps(notification_data)
+    )
     assert response.status_code == 200
     assert response.json().keys() == notification_response.keys()
     assert filter_response(response.json()) == filter_response(notification_response)
