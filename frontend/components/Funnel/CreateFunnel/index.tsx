f32e48af1de6ad76fbed90739ae115ca3d62497a
anish-parallel <111956747+anish-parallel@users.noreply.github.com>
2022-11-17 13:11:49 +0530

    Revert "Revert "[AP-145]:Create Funnel""

--- frontend/components/Funnel/CreateFunnel/index.tsx
+++ frontend/components/Funnel/CreateFunnel/index.tsx
@@ -1,42 +1,82 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Flex, Spinner, Text } from '@chakra-ui/react';
 import 'remixicon/fonts/remixicon.css';
-import FunnelImage from '@assets/images/funnel.svg';
-import Image from 'next/image';
-import LeftAction from './LeftAction';
+import CreateFunnelAction from './CreateFunnelAction';
 import RightPanel from '@components/EventsLayout/RightPanel';
+import FunnelChart from '../components/FunnelChart';
+import { useContext, useEffect, useState } from 'react';
+import { getCountOfValidAddedSteps } from '../util';
+import { MapContext } from '@lib/contexts/mapContext';
+import FunnelEmptyState from '../components/FunnelEmptyState';
+import { BLACK_RUSSIAN } from '@theme/index';
 import Render from '@components/Render';
 
 const Funnel = () => {
+  const {
+    state: { nodes },
+  } = useContext(MapContext);
+
+  const [funnelName, setFunnelName] = useState('Untitled Funnel');
+  const [funnelSteps, setFunnelSteps] = useState([
+    { event: '', filters: [] },
+    { event: '', filters: [] },
+  ]);
+  const [funnelData, setFunnelData] = useState([]);
+
+  const [isEmpty, setIsEmpty] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (getCountOfValidAddedSteps(funnelSteps, nodes) >= 2) {
+      setIsEmpty(false);
+    } else {
+      setIsEmpty(true);
+    }
+  }, [funnelSteps, nodes]);
+
+  useEffect(() => {
+    if (funnelData.length) {
+      setIsLoading(false);
+    } else setIsLoading(true);
+  }, [funnelData]);
+
   return (
     <Flex w={'full'} height={'full'}>
-      <LeftAction />
+      <CreateFunnelAction
+        funnelName={funnelName}
+        setFunnelName={setFunnelName}
+        funnelSteps={funnelSteps}
+        setFunnelSteps={setFunnelSteps}
+        setFunnelData={setFunnelData}
+      />
       <Render on="desktop">
         <RightPanel>
-          <Flex
-            h={'full'}
-            alignItems={'center'}
-            justifyContent={'center'}
-            px={'6'}
-            py={'6'}
-          >
-            <Box>
-              <Image
-                src={FunnelImage}
-                priority={true}
-                alt={'funnel-empty-state'}
-              />
-              <Text
-                textAlign={'center'}
-                mt={'10'}
-                fontSize={'sh-20'}
-                lineHeight={'sh-20'}
-                fontWeight={'normal'}
-                color={'grey.100'}
-              >
-                Enter events to create a funnel
+          {isEmpty ? (
+            <FunnelEmptyState />
+          ) : (
+            <Flex px={'30'} py={'30'} direction={'column'} gap={'8'} h={'full'}>
+              <Text fontSize={'sh-20'} fontWeight={'semibold'}>
+                Funnel
               </Text>
-            </Box>
-          </Flex>
+              {isLoading ? (
+                <Flex
+                  w="full"
+                  h="full"
+                  justifyContent={'center'}
+                  alignItems={'center'}
+                >
+                  <Spinner
+                    thickness="4px"
+                    speed="0.5s"
+                    emptyColor="gray.200"
+                    color={BLACK_RUSSIAN}
+                    size="xl"
+                  />
+                </Flex>
+              ) : (
+                <FunnelChart data={funnelData} />
+              )}
+            </Flex>
+          )}
         </RightPanel>
       </Render>
     </Flex>
