715a6085dabdf758c5185412bcda4105b8cea755
rnaidu <rahul@parallelhq.com>
2022-11-22 13:11:58 +0530

    Updated funnel queries to use pypika query builder

--- backend/repositories/clickhouse/funnels.py
+++ backend/repositories/clickhouse/funnels.py
@@ -1,4 +1,17 @@
 import logging
+from pypika import (
+    Table,
+    ClickHouseQuery,
+    Criterion,
+    AliasedQuery,
+    Case,
+    NULL,
+    DatePart,
+    Parameter,
+    CustomFunction,
+)
+from pypika import functions as fn
+from pypika.functions import Extract
 from fastapi import Depends
 from typing import List, Tuple, Dict
 
@@ -10,99 +23,122 @@ class Funnels:
     def __init__(self, clickhouse: Clickhouse = Depends()):
         self.clickhouse = clickhouse
         self.table = "events"
-        self.uid = "user_id"
+        self.epoch_year = 1970
 
     def execute_get_query(self, query: str, parameters: Dict):
+        logging.info(f"Executing query: {query}")
         query_result = self.clickhouse.client.query(query=query, parameters=parameters)
         return query_result.result_set
 
-    def get_conversion_data(self, ds_id: str, steps: List[FunnelStep]) -> List[Tuple]:
-        return self.execute_get_query(*self.trends_query_builder(ds_id, steps))
+    def get_conversion_trend(self, ds_id: str, steps: List[FunnelStep]) -> List[Tuple]:
+        return self.execute_get_query(*self.build_trends_query(ds_id, steps))
 
-    def get_events_data(self, ds_id: str, steps: List[FunnelStep]) -> List[Tuple]:
-        return self.execute_get_query(*self.users_query_builder(ds_id, steps))
+    def get_users_count(self, ds_id: str, steps: List[FunnelStep]) -> List[Tuple]:
+        return self.execute_get_query(*self.build_users_query(ds_id, steps))
 
-    def users_query_builder(self, ds_id: str, steps: List[FunnelStep]):
-        parameters = {"ds_id": ds_id, "epoch_year": 1970}
-
-        static_components = [
-            "with ",
-            f" select count(distinct table1.{self.uid}), ",
-            " from table1 ",
-        ]
-
-        dynamic_components_1, dynamic_components_2, dynamic_components_3 = [], [], []
+    def build_users_query(self, ds_id: str, steps: List[FunnelStep]):
+        query = ClickHouseQuery
+        events = Table(self.table)
+        parameters = {"ds_id": ds_id, "epoch_year": self.epoch_year}
 
         for i, step in enumerate(steps):
             parameters[f"event{i}"] = step.event
-            cte1 = f"table{i+1} as (select {self.uid}, min(timestamp) as ts from {self.table} where datasource_id=%(ds_id)s and event_name=%(event{i})s "
-            if step.filters:
-                for j, filter in enumerate(step.filters):
-                    parameters[f"filter{i}{j}"] = filter.value
-                    cte1 += f"and properties.{filter.property}=%(filter{i}{j})s "
-            cte1 += "group by 1)"
-            dynamic_components_1.append(cte1)
+            sub_query = (
+                ClickHouseQuery.from_(events)
+                .select(
+                    events.user_id,
+                    fn.Min(events.timestamp).as_("ts"),
+                )
+                .where(
+                    Criterion.all(
+                        [
+                            events.datasource_id == Parameter("%(ds_id)s"),
+                            events.event_name == Parameter(f"%(event{i})s"),
+                        ]
+                    )
+                )
+                .groupby(1)
+            )
+            query = query.with_(sub_query, f"table{i + 1}")
 
-            if i > 0:
-                ts_conditionals = ""
-                for j in range(i, 0, -1):
-                    ts_conditionals += f"table{j+1}.ts > table{j}.ts and "
+        query = query.from_(AliasedQuery("table1"))
+        for i in range(1, len(steps)):
+            query = query.left_join(AliasedQuery(f"table{i + 1}")).on_field("user_id")
 
-                cte2 = f"count(case when({ts_conditionals}year(table{i}.ts) > %(epoch_year)s) then table{i + 1}.user_id else null end)"
-                cte3 = f"left join table{i + 1} on (table{i}.{self.uid}=table{i + 1}.{self.uid}) "
-                dynamic_components_2.append(cte2)
-                dynamic_components_3.append(cte3)
+        query = query.select(fn.Count(AliasedQuery("table1").user_id).distinct())
+        for i in range(1, len(steps)):
+            conditions = [
+                Extract(DatePart.year, AliasedQuery(f"table{i}").ts)
+                > Parameter("%(epoch_year)s")
+            ]
+            for j in range(i, 0, -1):
+                conditions.append(
+                    AliasedQuery(f"table{j + 1}").ts > AliasedQuery(f"table{j}").ts
+                )
+            query = query.select(
+                fn.Count(
+                    Case()
+                    .when(
+                        Criterion.all(conditions),
+                        AliasedQuery(f"table{i + 1}").user_id,
+                    )
+                    .else_(NULL),
+                )
+            )
 
-        query = static_components[0]
-        query += ", ".join(dynamic_components_1)
-        query += static_components[1]
-        query += ", ".join(dynamic_components_2)
-        query += static_components[2]
-        query += " ".join(dynamic_components_3)
+        return query.get_sql(), parameters
 
-        logging.info(f"Executing query: {query}")
-        return query, parameters
-
-    def trends_query_builder(self, ds_id: str, steps: List[FunnelStep]):
-        parameters = {"ds_id": ds_id, "epoch_year": 1970}
-
-        static_components = [
-            "with ",
-            f" select week(table1.ts), year(table1.ts), ",
-            f" count(distinct table1.{self.uid}) as conversion ",
-            " from table1 ",
-            " group by 1, 2",
-        ]
-
-        dynamic_components_1, dynamic_components_2, dynamic_components_3 = [], [], []
+    def build_trends_query(self, ds_id: str, steps: List[FunnelStep]):
+        query = ClickHouseQuery
+        events = Table(self.table)
+        parameters = {"ds_id": ds_id, "epoch_year": self.epoch_year}
+        week_func = CustomFunction("WEEK", ["timestamp"])
 
         for i, step in enumerate(steps):
             parameters[f"event{i}"] = step.event
-            cte1 = f"table{i+1} as (select {self.uid}, min(timestamp) as ts from {self.table} where datasource_id=%(ds_id)s and event_name=%(event{i})s "
-            if step.filters:
-                for j, filter in enumerate(step.filters):
-                    parameters[f"filter{i}{j}"] = filter.value
-                    cte1 += f"and properties.{filter.property}=%(filter{i}{j})s "
-            cte1 += "group by 1)"
-            dynamic_components_1.append(cte1)
+            sub_query = (
+                ClickHouseQuery.from_(events)
+                .select(
+                    events.user_id,
+                    fn.Min(events.timestamp).as_("ts"),
+                )
+                .where(
+                    Criterion.all(
+                        [
+                            events.datasource_id == Parameter("%(ds_id)s"),
+                            events.event_name == Parameter(f"%(event{i})s"),
+                        ]
+                    )
+                )
+                .groupby(1)
+            )
+            query = query.with_(sub_query, f"table{i + 1}")
 
-            if i > 0:
-                ts_conditionals = ""
-                for j in range(i, 0, -1):
-                    ts_conditionals += f"table{j+1}.ts > table{j}.ts and "
+        query = query.from_(AliasedQuery("table1"))
+        conditions = [
+            Extract(DatePart.year, AliasedQuery(f"table{len(steps) - 1}").ts)
+            > Parameter("%(epoch_year)s")
+        ]
+        for i in range(1, len(steps)):
+            query = query.left_join(AliasedQuery(f"table{i + 1}")).on_field("user_id")
+            conditions.append(
+                AliasedQuery(f"table{i + 1}").ts >= AliasedQuery(f"table{i}").ts
+            )
 
-                cte2 = f"count(case when({ts_conditionals}year(table{i}.ts) > %(epoch_year)s) then table{i + 1}.user_id else null end)"
-                cte3 = f"left join table{i + 1} on (table{i}.{self.uid}=table{i + 1}.{self.uid}) "
-                dynamic_components_2.append(cte2)
-                dynamic_components_3.append(cte3)
+        query = query.select(
+            week_func(AliasedQuery("table1").ts),
+            Extract(DatePart.year, AliasedQuery("table1").ts),
+        )
+        query = query.select(
+            fn.Count(
+                Case()
+                .when(
+                    Criterion.all(conditions),
+                    AliasedQuery(f"table{len(steps)}").user_id,
+                )
+                .else_(NULL),
+            )
+            / fn.Count(AliasedQuery("table1").user_id).distinct()
+        ).groupby(1, 2)
 
-        query = static_components[0]
-        query += ", ".join(dynamic_components_1)
-        query += static_components[1]
-        query += dynamic_components_2[-1] + "/" + static_components[2]
-        query += static_components[3]
-        query += " ".join(dynamic_components_3)
-        query += static_components[4]
-
-        logging.info(f"Executing query: {query}")
-        return query, parameters
+        return query.get_sql(), parameters
