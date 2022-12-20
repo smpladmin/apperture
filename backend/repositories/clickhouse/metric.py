e6296343255d870324d54b555764b39199302979
Aditya Priyam <aditya@parallelhq.com>
2022-12-20 18:27:30 +0530

    starting with metric repository

---
+++ backend/repositories/clickhouse/metric.py
@@ -0,0 +1,10 @@
+
+from repositories.clickhouse.segments import Segments
+
+class Metrics(Segments):
+    def get_metric(self,datasource_id:str,):
+        pass
+
+    def build_segment_or_event_query(self):
+        pass
+
