a96ef8758fd94bd5b3cf19809a39cab27a5cb23c
rnaidu <rahul@parallelhq.com>
2023-01-02 16:48:15 +0530

    Added unit tests for multiple group segments

--- backend/tests/repositories/clickhouse/test_segments.py
+++ backend/tests/repositories/clickhouse/test_segments.py
@@ -38,6 +38,24 @@ class TestSegmentsRepository:
                 condition=SegmentFilterConditions.AND,
             ),
         ]
+        self.where_select_all_filters = [
+            WhereSegmentFilter(
+                operator=SegmentFilterOperators.EQUALS,
+                operand="prop1",
+                values=["va1", "val2"],
+                all=False,
+                type=SegmentFilterConditions.WHERE,
+                condition=SegmentFilterConditions.WHERE,
+            ),
+            WhereSegmentFilter(
+                operator=SegmentFilterOperators.EQUALS,
+                operand="prop2",
+                values=["va3", "val4"],
+                all=True,
+                type=SegmentFilterConditions.WHERE,
+                condition=SegmentFilterConditions.AND,
+            ),
+        ]
         self.composite_filters = [
             WhereSegmentFilter(
                 operator=SegmentFilterOperators.EQUALS,
@@ -94,6 +112,30 @@ class TestSegmentsRepository:
                 condition=SegmentFilterConditions.AND,
             ),
         ]
+        self.who_filters_with_dates = [
+            WhoSegmentFilter(
+                operand="Topic_Click",
+                operator=SegmentFilterOperators.EQUALS,
+                values=["2"],
+                triggered=True,
+                aggregation="total",
+                type=SegmentFilterConditions.WHO,
+                condition=SegmentFilterConditions.WHO,
+                start_date="2022-01-01",
+                end_date="2023-01-01",
+            ),
+            WhoSegmentFilter(
+                operand="Video_Open",
+                operator=SegmentFilterOperators.EQUALS,
+                values=["3"],
+                triggered=False,
+                aggregation="total",
+                type=SegmentFilterConditions.WHO,
+                condition=SegmentFilterConditions.AND,
+                start_date="2022-01-01",
+                end_date="2023-01-01",
+            ),
+        ]
         self.groups = [
             SegmentGroup(filters=self.filters, condition=SegmentGroupConditions.AND),
             SegmentGroup(
@@ -103,7 +145,7 @@ class TestSegmentsRepository:
                 filters=self.composite_filters, condition=SegmentGroupConditions.AND
             ),
         ]
-        self.columns = ["prop1", "prop2", "prop3"]
+        self.columns = ["col1", "col2", "col3"]
         self.params = {"ds_id": "test-id"}
         self.where_filters_query = (
             'SELECT DISTINCT "user_id" FROM "events" WHERE "datasource_id"=%(ds_id)s AND '
@@ -218,10 +260,10 @@ class TestSegmentsRepository:
             ),
         ) == (
             (
-                'WITH column_data AS (SELECT "user_id","timestamp","properties.prop1",RANK() '
+                'WITH column_data AS (SELECT "user_id","timestamp","properties.col1",RANK() '
                 'OVER(PARTITION BY "user_id" ORDER BY "timestamp" DESC) AS "Rank" FROM '
                 '"events" WHERE "datasource_id"=%(ds_id)s AND '
-                'char_length(toString("properties.prop1"))>0 AND "user_id" IN (WITH group0 AS '
+                'char_length(toString("properties.col1"))>0 AND "user_id" IN (WITH group0 AS '
                 '(WITH cte0 AS (SELECT "user_id" FROM "events" WHERE '
                 '"datasource_id"=%(ds_id)s AND "user_id" IN (SELECT DISTINCT "user_id" FROM '
                 '"events" WHERE "datasource_id"=%(ds_id)s) AND "event_name"=\'Topic_Click\' '
@@ -231,7 +273,7 @@ class TestSegmentsRepository:
                 '"event_name"<>\'Video_Open\') SELECT DISTINCT "cte0"."user_id" FROM cte0 '
                 'INTERSECT SELECT DISTINCT "cte1"."user_id" FROM cte1) SELECT DISTINCT '
                 '"group0"."user_id" FROM group0) ORDER BY "user_id") SELECT '
-                '"properties.prop1","user_id" FROM column_data WHERE "Rank"=1 ORDER BY '
+                '"properties.col1","user_id" FROM column_data WHERE "Rank"=1 ORDER BY '
                 '"user_id"'
             )
         )
@@ -270,3 +312,92 @@ class TestSegmentsRepository:
         ]
 
         self.repo.execute_get_query.assert_has_calls(calls=calls, any_order=True)
+
+    def test_get_segment_data_for_multiple_groups(self):
+        self.repo.execute_get_query = MagicMock()
+        self.repo.get_segment_data(
+            datasource_id=self.datasource_id,
+            groups=self.groups,
+            columns=self.columns,
+        )
+        calls = [
+            call(
+                parameters=self.params,
+                query=(
+                    "WITH column_data AS (SELECT "
+                    f'"user_id","timestamp","properties.{column}",RANK() OVER(PARTITION BY '
+                    '"user_id" ORDER BY "timestamp" DESC) AS "Rank" FROM "events" WHERE '
+                    '"datasource_id"=%(ds_id)s AND '
+                    f'char_length(toString("properties.{column}"))>0 AND "user_id" IN (WITH '
+                    'group0 AS (SELECT DISTINCT "user_id" FROM "events" WHERE '
+                    '"datasource_id"=%(ds_id)s AND "properties.prop1" IN '
+                    "('va1','val2') AND \"properties.prop2\" IN ('va3','val4')) "
+                    ',group1 AS (WITH cte0 AS (SELECT "user_id" FROM "events" WHERE '
+                    '"datasource_id"=%(ds_id)s AND "user_id" IN (SELECT DISTINCT '
+                    '"user_id" FROM "events" WHERE "datasource_id"=%(ds_id)s) AND '
+                    '"event_name"=\'Topic_Click\' GROUP BY "user_id" HAVING '
+                    'COUNT("user_id")=\'2\') ,cte1 AS (SELECT "user_id" FROM "events" '
+                    'WHERE "datasource_id"=%(ds_id)s AND "user_id" IN (SELECT DISTINCT '
+                    '"user_id" FROM "events" WHERE "datasource_id"=%(ds_id)s) AND '
+                    '"event_name"<>\'Video_Open\') SELECT DISTINCT "cte0"."user_id" FROM '
+                    'cte0 INTERSECT SELECT DISTINCT "cte1"."user_id" FROM cte1) ,group2 '
+                    'AS (WITH cte2 AS (SELECT "user_id" FROM "events" WHERE '
+                    '"datasource_id"=%(ds_id)s AND "user_id" IN (SELECT DISTINCT '
+                    '"user_id" FROM "events" WHERE "datasource_id"=%(ds_id)s AND '
+                    "\"properties.prop1\" IN ('va1','val2') AND \"properties.prop2\" IN "
+                    "('va3','val4')) AND \"event_name\"='Topic_Click' GROUP BY "
+                    '"user_id" HAVING COUNT("user_id")=\'2\') ,cte3 AS (SELECT "user_id" '
+                    'FROM "events" WHERE "datasource_id"=%(ds_id)s AND "user_id" IN '
+                    '(SELECT DISTINCT "user_id" FROM "events" WHERE '
+                    '"datasource_id"=%(ds_id)s AND "properties.prop1" IN '
+                    "('va1','val2') AND \"properties.prop2\" IN ('va3','val4')) "
+                    'AND "event_name"<>\'Video_Open\') SELECT DISTINCT "cte2"."user_id" '
+                    'FROM cte2 UNION ALL SELECT DISTINCT "cte3"."user_id" FROM cte3) '
+                    'SELECT DISTINCT "group0"."user_id" FROM group0 INTERSECT SELECT '
+                    'DISTINCT "group1"."user_id" FROM group1 INTERSECT SELECT DISTINCT '
+                    '"group2"."user_id" FROM group2) ORDER BY "user_id") SELECT '
+                    f'"properties.{column}","user_id" FROM column_data WHERE "Rank"=1 ORDER '
+                    'BY "user_id"'
+                ),
+            )
+            for column in self.columns
+        ]
+
+        self.repo.execute_get_query.assert_has_calls(calls=calls, any_order=True)
+
+    def test_build_segment_users_query_for_where_filters_select_all(self):
+        assert self.repo.build_segment_users_query(
+            groups=[
+                SegmentGroup(
+                    filters=self.where_select_all_filters,
+                    condition=SegmentGroupConditions.AND,
+                )
+            ]
+        ).get_sql() == (
+            'WITH group0 AS (SELECT DISTINCT "user_id" FROM "events" WHERE '
+            "\"datasource_id\"=%(ds_id)s AND \"properties.prop1\" IN ('va1','val2')) "
+            'SELECT DISTINCT "group0"."user_id" FROM group0'
+        )
+
+    def test_build_who_clause_users_query_for_who_filters_with_date(self):
+        segment_users = self.repo.get_all_unique_users_query()
+        segment_users = self.repo.build_who_clause_users_query(
+            group=SegmentGroup(
+                filters=self.who_filters_with_dates,
+                condition=SegmentGroupConditions.AND,
+            ),
+            group_users=segment_users,
+            idx=0,
+        )
+        assert segment_users.get_sql() == (
+            'WITH cte0 AS (SELECT "user_id" FROM "events" WHERE "datasource_id"=%(ds_id)s '
+            'AND "user_id" IN (SELECT DISTINCT "user_id" FROM "events" WHERE '
+            '"datasource_id"=%(ds_id)s) AND DATE("timestamp")>=\'2022-01-01\' AND '
+            "DATE(\"timestamp\")<='2023-01-01' AND \"event_name\"='Topic_Click' GROUP BY "
+            '"user_id" HAVING COUNT("user_id")=\'2\') ,cte1 AS (SELECT "user_id" FROM '
+            '"events" WHERE "datasource_id"=%(ds_id)s AND "user_id" IN (SELECT DISTINCT '
+            '"user_id" FROM "events" WHERE "datasource_id"=%(ds_id)s) AND '
+            "DATE(\"timestamp\")>='2022-01-01' AND DATE(\"timestamp\")<='2023-01-01' AND "
+            '"event_name"<>\'Video_Open\') SELECT DISTINCT "cte0"."user_id" FROM cte0 '
+            'INTERSECT SELECT DISTINCT "cte1"."user_id" FROM cte1'
+        )
