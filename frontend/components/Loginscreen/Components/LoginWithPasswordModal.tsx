814e52408e31fbbe897eac2f43c5c37084b5c77d
Aditya Priyam <aditya@parallelhq.com>
2023-02-09 00:40:57 +0530

    Login Modal complete

--- frontend/components/Loginscreen/Components/LoginWithPasswordModal.tsx
+++ frontend/components/Loginscreen/Components/LoginWithPasswordModal.tsx
@@ -2,6 +2,7 @@ import {
   Button,
   Flex,
   FormControl,
+  FormLabel,
   Input,
   Modal,
   ModalBody,
@@ -11,8 +12,8 @@ import {
   ModalHeader,
   ModalOverlay,
 } from '@chakra-ui/react';
-import React, { useState } from 'react';
-import Image from 'next/image';
+import React, { useRef, useState } from 'react';
+import { BACKEND_BASE_URL } from 'config';
 
 const LoginWithPasswordModal = ({
   isOpen = true,
@@ -23,6 +24,7 @@ const LoginWithPasswordModal = ({
 }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const redirectionRef = useRef<any>();
 
   const handleEmail = (e: any) => {
     setEmail(e.target.value);
@@ -30,29 +32,63 @@ const LoginWithPasswordModal = ({
   const handlePassword = (e: any) => {
     setPassword(e.target.value);
   };
+
+  const handleLogin = (e: any) => {
+    e.preventDefault();
+    if (email && password) redirectionRef.current.click();
+  };
+
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Login</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          <form id="login-form" onSubmit={handleLogin}>
-            <FormControl>
-              <Flex gap={2} direction="column">
-                <Input type="email" placeholder="Email" />
-                <Input type="password" placeholder="Password" />
-              </Flex>
-            </FormControl>
-          </form>
-        </ModalBody>
-        <ModalFooter>
-          <Button type={'submit'} form={'login-form'} variant={'secondary'}>
-            Log In
-          </Button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+    <>
+      <a
+        ref={redirectionRef}
+        href={`${BACKEND_BASE_URL}/login/password?email=${email}&password=${password}`}
+      >
+        {''}
+      </a>
+      <Modal
+        isCentered
+        motionPreset="slideInBottom"
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay backdropFilter={'blur(10px)'} bg={'grey.0'} />
+        <ModalContent>
+          <ModalHeader></ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <form id="login-form" onSubmit={handleLogin}>
+              <FormControl>
+                <Flex gap={2} direction="column" pt={5}>
+                  <FormLabel>Email</FormLabel>
+                  <Input
+                    type="email"
+                    placeholder="John@doe.com"
+                    onChange={handleEmail}
+                  />
+                  <FormLabel mt={2}>Password</FormLabel>
+                  <Input
+                    type="password"
+                    placeholder="xxxxxxxxxxxx"
+                    onChange={handlePassword}
+                  />
+                </Flex>
+              </FormControl>
+            </form>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              borderWidth={'1px'}
+              type={'submit'}
+              form={'login-form'}
+              variant={'secondary'}
+            >
+              Log In
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
