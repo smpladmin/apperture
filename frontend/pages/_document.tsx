357d8f5a0ace7d364ed6c5737c0c3ccf613290e3
Aman <aman@parallelhq.com>
2023-03-21 15:05:08 +0530

    use gstatic to load fonts

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -9,6 +9,11 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon/favicon.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
             href="https://fonts.googleapis.com/css?family=Inter:wght@100;200;300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
