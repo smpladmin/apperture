ff797623864ba72883fd33365b23935add3159fd
rnaidu <rahul@parallelhq.com>
2023-03-27 16:23:11 +0530

    removed hardcoded db_name variable

--- backend/run_mongo_migrations.sh
+++ backend/run_mongo_migrations.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Running migration for mongo db"
-source .env || true && beanie migrate -uri $DB_URI -p mongo_migrations -db apperture_db --distance 1
+source .env && beanie migrate -uri $DB_URI -p mongo_migrations -db $DB_NAME --distance 1
