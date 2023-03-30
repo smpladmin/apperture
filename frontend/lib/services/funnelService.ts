9a4eaa7bbec4cae826d6c95a8f25ec98ed64c8b1
Aditya Priyam <aditya@parallelhq.com>
2023-03-30 12:00:06 +0530

    funnel integration with random sequence

--- frontend/lib/services/funnelService.ts
+++ frontend/lib/services/funnelService.ts
@@ -103,7 +103,8 @@ export const getConversionData = async (
   steps: FunnelStep[],
   status: ConversionStatus,
   dateFilter: DateFilterObj,
-  conversionWindow: ConversionWindowObj
+  conversionWindow: ConversionWindowObj,
+  randomSequence: boolean
 ) => {
   const res = await ApperturePost('/funnels/analytics/transient', {
     datasourceId: dsId,
@@ -111,6 +112,7 @@ export const getConversionData = async (
     status,
     dateFilter,
     conversionWindow,
+    randomSequence,
   });
   return res.data || [];
 };
