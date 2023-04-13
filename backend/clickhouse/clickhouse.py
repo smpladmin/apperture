8b47ff4dc1775159475ac15698128f778dde2863
Roopak Nijhara <roopak@parallelhq.com>
2023-04-13 17:29:48 +0530

    clickhouse-connect version fix

--- backend/clickhouse/clickhouse.py
+++ backend/clickhouse/clickhouse.py
@@ -5,6 +5,7 @@ class Clickhouse:
     def init(self):
         self.client = clickhouse_connect.get_client(
             host="clickhouse",
+            allow_experimental_object_type=1,
             query_limit=0,
         )
 
