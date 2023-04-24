38e6db852b4d7ee0ce9d9f8dc9777de408f6cfb2
Aman <aman@parallelhq.com>
2023-04-24 06:28:42 +0530

    view page created

---
+++ frontend/pages/analytics/retention/view/[rententionid].tsx
@@ -0,0 +1,85 @@
+import ViewFunnelComponent from '@components/Funnel/ViewFunnel';
+import Layout from '@components/Layout';
+import { AppWithIntegrations } from '@lib/domain/app';
+import { Funnel } from '@lib/domain/funnel';
+import { Notifications } from '@lib/domain/notification';
+import { _getAppsWithIntegrations } from '@lib/services/appService';
+import { _getSavedFunnel } from '@lib/services/funnelService';
+import { _getNotificationByReference } from '@lib/services/notificationService';
+import { getAuthToken } from '@lib/utils/request';
+import { GetServerSideProps } from 'next';
+import { ReactElement } from 'react';
+
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  query,
+}) => {
+  const token = getAuthToken(req);
+  if (!token) {
+    return {
+      props: {},
+    };
+  }
+
+  const { funnelId, dsId } = query;
+  const apps = await _getAppsWithIntegrations(token);
+  const savedFunnel = await _getSavedFunnel(token, funnelId as string);
+
+  const datasourceId = dsId || savedFunnel.datasourceId;
+  const savedNotification =
+    (await _getNotificationByReference(
+      token,
+      funnelId as string,
+      datasourceId as string
+    )) || {};
+
+  if (!apps.length) {
+    return {
+      redirect: {
+        destination: '/analytics/app/create',
+      },
+      props: {},
+    };
+  }
+
+  if (!savedFunnel) {
+    return {
+      redirect: {
+        destination: '/404',
+      },
+      props: {},
+    };
+  }
+
+  return {
+    props: { apps, savedFunnel, savedNotification },
+  };
+};
+
+const ViewRetention = ({
+  savedFunnel,
+  savedNotification,
+}: {
+  savedFunnel: Funnel;
+  savedNotification: Notifications;
+}) => {
+  return (
+    <ViewFunnelComponent
+      savedFunnel={savedFunnel}
+      savedNotification={savedNotification}
+    />
+  );
+};
+
+ViewRetention.getLayout = function getLayout(
+  page: ReactElement,
+  apps: AppWithIntegrations[]
+) {
+  return (
+    <Layout apps={apps} hideHeader={true}>
+      {page}
+    </Layout>
+  );
+};
+
+export default ViewRetention;
