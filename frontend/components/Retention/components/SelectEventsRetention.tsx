8e6b562b88a010c77379d1a97ab8772cfb6b5f2e
rnaidu <rahul@parallelhq.com>
2023-04-26 18:35:23 +0530

    Added filters for create retention action

--- frontend/components/Retention/components/SelectEventsRetention.tsx
+++ frontend/components/Retention/components/SelectEventsRetention.tsx
@@ -5,8 +5,27 @@ import { MapContext } from '@lib/contexts/mapContext';
 import { FunnelStep } from '@lib/domain/funnel';
 import { RetentionEvents } from '@lib/domain/retention';
 import { useOnClickOutside } from '@lib/hooks/useOnClickOutside';
-import React, { useContext, useRef, useState } from 'react';
+import React, {
+  Fragment,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { Node } from '@lib/domain/node';
+import StepFilter from '@components/StepFilters/StepFilters';
+import AddFilterComponent from '@components/StepFilters/AddFilter';
+import { getEventProperties } from '@lib/services/datasourceService';
+import { useRouter } from 'next/router';
+import {
+  FilterConditions,
+  FilterDataType,
+  FilterOperatorsString,
+  FilterType,
+  WhereFilter,
+} from '@lib/domain/common';
+import { cloneDeep } from 'lodash';
 
 type SelectEventsRetentionProps = {
   retentionEvent: FunnelStep;
@@ -28,11 +47,18 @@ const SelectEventsRetention = ({
   } = useContext(MapContext);
 
   const eventDropdownRef = useRef(null);
+  const router = useRouter();
+  const { dsId } = router.query;
 
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   useOnClickOutside(eventDropdownRef, () => {
     setIsDropdownOpen(false);
   });
+
+  const [eventProperties, setEventProperties] = useState<string[]>([]);
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+  const [loadingEventProperties, setLoadingEventProperties] =
+    useState<boolean>(false);
 
   const handleEventSelection = (event: Node) => {
     const existingFilters = retentionEvents[eventKey].filters;
@@ -45,9 +71,86 @@ const SelectEventsRetention = ({
     setIsDropdownOpen(false);
   };
 
+  useEffect(() => {
+    const fetchEventProperties = async () => {
+      const properties = await getEventProperties(dsId as string);
+      setEventProperties(properties);
+      setLoadingEventProperties(false);
+    };
+
+    setLoadingEventProperties(true);
+    fetchEventProperties();
+  }, []);
+
+  const updateEventFilters = useCallback(
+    (stepFilters: WhereFilter[]) => {
+      const tempRetentionEvents = cloneDeep(retentionEvents);
+      tempRetentionEvents[eventKey].filters = stepFilters;
+      setRetentionEvents(tempRetentionEvents);
+    },
+    [retentionEvents]
+  );
+
+  const handleSetFilterProperty = (filterIndex: number, property: string) => {
+    let stepFilters = [...retentionEvent.filters];
+    stepFilters[filterIndex]['operand'] = property;
+
+    updateEventFilters(stepFilters);
+  };
+
+  const handleSetFilterValue = (filterIndex: number, values: string[]) => {
+    let stepFilters = [...retentionEvent.filters];
+    stepFilters[filterIndex]['values'] = values;
+
+    updateEventFilters(stepFilters);
+  };
+
+  const handleRemoveFilter = (filterIndex: number) => {
+    let stepFilters = [...retentionEvent.filters];
+    stepFilters.splice(filterIndex, 1);
+
+    if (filterIndex === 0 && stepFilters.length)
+      stepFilters[0]['condition'] = FilterConditions.WHERE;
+
+    updateEventFilters(stepFilters);
+  };
+
+  const handleAddFilter = (value: string) => {
+    const getFilterCondition = (stepFilters: WhereFilter[]) => {
+      return !stepFilters.length
+        ? FilterConditions.WHERE
+        : FilterConditions.AND;
+    };
+
+    const stepFilters = [...retentionEvent.filters];
+    stepFilters.push({
+      condition: getFilterCondition(stepFilters),
+      operand: value,
+      operator: FilterOperatorsString.IS,
+      values: [],
+      type: FilterType.WHERE,
+      all: false,
+      datatype: FilterDataType.STRING,
+    });
+
+    updateEventFilters(stepFilters);
+  };
+
   return (
-    <Flex gap={3} flexDirection="column">
-      <Card p={3} borderRadius={'8'}>
+    <Flex
+      p={'3'}
+      borderRadius={'8px'}
+      border={'1px'}
+      borderColor={isHovered ? 'grey.700 ' : 'white.200'}
+      direction={'column'}
+      cursor={'grab'}
+      data-testid={'funnel-step'}
+      backgroundColor={'white.DEFAULT'}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      gap={'2'}
+    >
+      <Flex gap={3} flexDirection="column">
         <Flex width={'full'}>
           <Flex
             width={'full'}
@@ -109,7 +212,32 @@ const SelectEventsRetention = ({
             </Flex>
           </Flex>
         </Flex>
-      </Card>
+        {Boolean(retentionEvent.filters.length) && (
+          <Flex direction={'column'} gap={'2'}>
+            {retentionEvent.filters.map((filter, index) => (
+              <Fragment key={index}>
+                <StepFilter
+                  index={index}
+                  filter={filter}
+                  eventProperties={eventProperties}
+                  loadingEventProperties={loadingEventProperties}
+                  handleSetFilterProperty={handleSetFilterProperty}
+                  handleSetFilterValue={handleSetFilterValue}
+                  handleRemoveFilter={handleRemoveFilter}
+                />
+              </Fragment>
+            ))}
+          </Flex>
+        )}
+        {retentionEvent.event ? (
+          <AddFilterComponent
+            filters={retentionEvent.filters}
+            eventProperties={eventProperties}
+            loadingEventProperties={loadingEventProperties}
+            handleAddFilter={handleAddFilter}
+          />
+        ) : null}
+      </Flex>
     </Flex>
   );
 };
