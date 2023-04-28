120e267ff4ecf428745ea66dc0eb1ded34f7e279
rnaidu <rahul@parallelhq.com>
2023-04-28 11:08:11 +0530

    Added filters & segment filters to retention query

--- backend/tests/repositories/clickhouse/test_retention.py
+++ backend/tests/repositories/clickhouse/test_retention.py
@@ -1,9 +1,11 @@
 from unittest.mock import MagicMock, ANY
 
 import pytest
-from pypika import Interval
+from pypika import ClickHouseQuery
 
+from domain.common.filter_models import FilterOperatorsString, FilterDataType
 from domain.retention.models import EventSelection, Granularity
+from domain.segments.models import WhereSegmentFilter, SegmentFilterConditions
 from repositories.clickhouse.retention import Retention
 
 
@@ -19,6 +21,9 @@ class TestRetentionRepository:
         self.start_event = EventSelection(event="start_event", filters=None)
         self.goal_event = EventSelection(event="goal_event", filters=None)
         self.granularity = Granularity.DAYS
+        self.segment_filter_criterion = self.repo.table.user_id.isin(
+            ClickHouseQuery.from_(self.repo.table).select("*")
+        )
         self.parameters = {
             "ds_id": "test-id",
             "end_date": "2022-12-31",
@@ -83,24 +88,76 @@ class TestRetentionRepository:
             self.repo.build_retention_trend_query(
                 granularity=self.granularity,
                 segment_filter_criterion=None,
+                start_event=self.start_event,
+                goal_event=self.goal_event,
             ).get_sql()
             == self.retention_trend_query
         )
 
-    @pytest.mark.parametrize("event_flag, result", [(True, (
+    @pytest.mark.parametrize(
+        "event_flag, event, result",
+        [
+            (
+                True,
+                EventSelection(event="start_event", filters=None),
+                (
+                    'SELECT "user_id",toStartOfInterval("timestamp",INTERVAL \'1 DAY\') AS '
+                    '"granularity",MIN("timestamp") AS "ts" FROM "events" WHERE '
+                    '"datasource_id"=%(ds_id)s AND DATE("timestamp")>=%(start_date)s AND '
+                    'DATE("timestamp")<=%(end_date)s AND "event_name"=%(start_event)s GROUP BY '
+                    "1,2"
+                ),
+            ),
+            (
+                False,
+                EventSelection(
+                    event="goal_event",
+                    filters=[
+                        WhereSegmentFilter(
+                            operator=FilterOperatorsString.IS,
+                            operand="properties.$city",
+                            values=["Bengaluru"],
+                            all=False,
+                            type=SegmentFilterConditions.WHERE,
+                            condition=SegmentFilterConditions.WHERE,
+                            datatype=FilterDataType.STRING,
+                        )
+                    ],
+                ),
+                (
+                    'SELECT "user_id",toStartOfInterval("timestamp",INTERVAL \'1 DAY\') AS '
+                    '"granularity",MAX("timestamp") AS "ts" FROM "events" WHERE '
+                    '"datasource_id"=%(ds_id)s AND DATE("timestamp")>=%(start_date)s AND '
+                    'DATE("timestamp")<=%(end_date)s AND "event_name"=%(goal_event)s AND '
+                    "\"properties.properties.$city\" IN ('Bengaluru') GROUP BY 1,2"
+                ),
+            ),
+        ],
+    )
+    def test_build_sub_query(self, event_flag, event, result):
+        assert (
+            self.repo.build_sub_query(
+                granularity=self.granularity,
+                event_flag=event_flag,
+                event=event,
+                segment_filter_criterion=None,
+            ).get_sql()
+            == result
+        )
+
+    def test_build_sub_query_with_segment_filter(self):
+        assert self.repo.build_sub_query(
+            granularity=self.granularity,
+            event_flag=True,
+            event=self.start_event,
+            segment_filter_criterion=self.segment_filter_criterion,
+        ).get_sql() == (
             'SELECT "user_id",toStartOfInterval("timestamp",INTERVAL \'1 DAY\') AS '
             '"granularity",MIN("timestamp") AS "ts" FROM "events" WHERE '
             '"datasource_id"=%(ds_id)s AND DATE("timestamp")>=%(start_date)s AND '
-            'DATE("timestamp")<=%(end_date)s AND "event_name"=%(start_event)s GROUP BY '
-            '1,2'
-    )), (False, (
-            'SELECT "user_id",toStartOfInterval("timestamp",INTERVAL \'1 DAY\') AS '
-            '"granularity",MAX("timestamp") AS "ts" FROM "events" WHERE '
-            '"datasource_id"=%(ds_id)s AND DATE("timestamp")>=%(start_date)s AND '
-            'DATE("timestamp")<=%(end_date)s AND "event_name"=%(goal_event)s GROUP BY 1,2'
-    ))])
-    def test_build_sub_query(self, event_flag, result):
-        assert self.repo.build_sub_query(granularity=self.granularity, event_flag=event_flag).get_sql() == result
+            'DATE("timestamp")<=%(end_date)s AND "event_name"=%(start_event)s AND '
+            '"user_id" IN (SELECT * FROM "events") GROUP BY 1,2'
+        )
 
     def test_compute_retention(self):
         self.repo.execute_get_query.return_value = [(0.5516,)]
