a0f2e24eae9d010bafc0b6d4f9f609cfb0ae6544
anish-parallel <anish@parallelhq.com>
2023-05-08 11:24:08 +0530

    refactor retention event filters

--- frontend/components/Retention/utils.ts
+++ frontend/components/Retention/utils.ts
@@ -1,7 +1,6 @@
 import { ExternalSegmentFilter, WhereFilter } from '@lib/domain/common';
 import { FunnelStep } from '@lib/domain/funnel';
 import { RetentionEvents } from '@lib/domain/retention';
-import { WhereSegmentFilter } from '@lib/domain/segment';
 import { isEveryCustomSegmentFilterValid } from '@lib/utils/common';
 
 const _hasValidFilterValues = (filters: WhereFilter[]) => {
@@ -43,7 +42,7 @@ export const hasValidRetentionEventAndFilters = (
     hasValidEvents(retentionEvents) &&
     hasValidFilterValuesForAllEvents(retentionEvents) &&
     isEveryCustomSegmentFilterValid(
-      segmentFilters[0].custom.filters as WhereSegmentFilter[]
+      segmentFilters[0].custom.filters as WhereFilter[]
     )
   );
 };
