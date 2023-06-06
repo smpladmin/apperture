83b69eed3799c1e06bba06f3bf92257ef15b06c7
anish-parallel <anish@parallelhq.com>
2023-06-06 17:31:50 +0530

    add methods to create user and grant permissions

--- backend/repositories/clickhouse/role.py
+++ backend/repositories/clickhouse/role.py
@@ -1,12 +1,23 @@
 from fastapi import Depends
 from clickhouse.clickhouse import Clickhouse
 
-from abc import ABC
+from repositories.clickhouse.base import EventsBase
 
 
-class EventsBase(ABC):
+class Role(EventsBase):
     def __init__(self, clickhouse: Clickhouse = Depends()):
         self.clickhouse = clickhouse
 
-    def createNewRole(self, username, password):
-        pass
+    def create_user(self, username: str, password: str):
+        query = (
+            f"Create user {username} IDENTIFIED WITH plaintext_password BY {password}"
+        )
+        return self.execute_get_query(query=query, parameters={})
+
+    def grant_select_permission_to_user(self, username: str):
+        query = f"Grant Select on events to {username}"
+        return self.execute_get_query(query=query, parameters={})
+
+    def create_row_policy(self, datasource_id: str, username: str):
+        query = f"CREATE ROW POLICY pol{datasource_id} ON events USING datasource_id={datasource_id} TO {username}"
+        return self.execute_get_query(query=query, parameters={})
