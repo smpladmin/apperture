0691a509459a2213fbf00ff21e1f2423ddc9993d
Aditya Priyam <aditya@parallelhq.com>
2023-06-09 11:23:30 +0530

    Refactor restricted client method

--- backend/clickhouse/clickhouse.py
+++ backend/clickhouse/clickhouse.py
@@ -1,4 +1,5 @@
 import os
+
 import clickhouse_connect
 
 
@@ -15,6 +16,15 @@ class Clickhouse:
             query_limit=0,
             username=os.getenv("CHDB_ADMIN_USERNAME", "clickhouse_admin"),
             password=os.getenv("CHDB_ADMIN_PASSWORD", "password"),
+        )
+
+    def get_connection_for_user(self, username: str, password: str):
+        return clickhouse_connect.get_client(
+            host="clickhouse",
+            allow_experimental_object_type=1,
+            query_limit=0,
+            username=username,
+            password=password,
         )
 
     def close(self):
--- backend/repositories/clickhouse/spreadsheet.py
+++ backend/repositories/clickhouse/spreadsheet.py
@@ -9,23 +9,14 @@ from repositories.clickhouse.base import EventsBase
 from repositories.clickhouse.parser.query_parser import QueryParser
 
 
-class Spreadsheets(EventsBase):
+class Spreadsheets:
     def __init__(
         self,
         clickhouse: Clickhouse = Depends(),
         parser: QueryParser = Depends(),
     ):
-        super().__init__(clickhouse=clickhouse)
+        self.clickhouse = clickhouse
         self.parser = parser
-
-    def get_connection_for_user(self, username: str, password: str):
-        return clickhouse_connect.get_client(
-            host="clickhouse",
-            allow_experimental_object_type=1,
-            query_limit=0,
-            username=username,
-            password=password,
-        )
 
     def get_transient_spreadsheet(
         self,
@@ -34,7 +25,7 @@ class Spreadsheets(EventsBase):
         password: Union[str, None],
     ):
         query = self.parser.assign_query_limit(query)
-        restricted_client = self.get_connection_for_user(
+        restricted_client = self.clickhouse.get_connection_for_user(
             username=username, password=password
         )
         result = restricted_client.query(query=query)
