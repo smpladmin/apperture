1dc26083c1b7caa07fc83966f51c3bb9bc1318a6
anish-parallel <anish@parallelhq.com>
2023-06-19 13:00:47 +0530

    fix warnig of getClientRects in frontend tests

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -35,3 +35,19 @@ jest.mock('@antv/g2plot', () => ({
 }));
 
 window.HTMLElement.prototype.scrollIntoView = jest.fn();
+
+document.createRange = () => {
+  const range = new Range();
+
+  range.getBoundingClientRect = jest.fn();
+
+  range.getClientRects = () => {
+    return {
+      item: () => null,
+      length: 0,
+      [Symbol.iterator]: jest.fn(),
+    };
+  };
+
+  return range;
+};
