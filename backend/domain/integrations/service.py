d16cd9a0256a16b8724f928854161380350c10ce
rnaidu <rahul@parallelhq.com>
2023-07-18 15:19:03 +0530

    MySQL connector: ssh tunnel

--- backend/domain/integrations/service.py
+++ backend/domain/integrations/service.py
@@ -1,6 +1,9 @@
 import logging
+import os
+import tempfile
 from typing import Optional
-import mysql.connector
+import pymysql
+import sshtunnel
 
 from beanie import PydanticObjectId
 from authorisation.models import IntegrationOAuth
@@ -124,21 +127,21 @@ class IntegrationService:
             ssh_credential=db_ssh_credential,
         )
 
-    def test_mysql_connection(
+    def check_mysql_connection(
         self, host: str, port: str, username: str, password: str
     ):
         connection_successful = False
         try:
             # Establish a connection to the MySQL server
-            connection = mysql.connector.connect(
+            connection = pymysql.connect(
                 host=host,
-                port=port,
+                port=int(port),
                 user=username,
                 password=password,
             )
 
             # Check if the connection was successful
-            if connection.is_connected():
+            if connection.open:
                 connection_successful = True
                 logging.info("Connected to MySQL database")
 
@@ -146,7 +149,59 @@ class IntegrationService:
             connection.close()
             logging.info("Connection closed")
 
-        except mysql.connector.Error as error:
-            logging.info(f"Failed to connect to MySQL database: {error}")
+        except Exception as e:
+            logging.info(f"Failed to connect to MySQL database with exception: {e}")
 
         return connection_successful
+
+    def create_temp_file(self, content: str):
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        temp_file.write(content.encode("utf-8"))
+        temp_file.flush()
+        return temp_file.name
+
+    def test_mysql_connection(
+        self,
+        host: str,
+        port: str,
+        username: str,
+        password: str,
+        ssh_credential: Optional[DatabaseSSHCredentialDto],
+    ):
+
+        if ssh_credential:
+            logging.info("SSH credentials exists")
+            ssh_pkey = None
+            if ssh_credential.sshKey:
+                logging.info("SSH key exists, creating temp file to store ssh key")
+                ssh_pkey = self.create_temp_file(ssh_credential.sshKey)
+                logging.info(f"Temporary file name: {ssh_pkey}")
+
+            try:
+                with sshtunnel.SSHTunnelForwarder(
+                    (ssh_credential.server, int(ssh_credential.port)),
+                    ssh_pkey=ssh_pkey,
+                    ssh_username=ssh_credential.username,
+                    ssh_password=ssh_credential.password,
+                    remote_bind_address=(host, int(port)),
+                ) as tunnel:
+                    logging.info(
+                        f"Created SSH tunnel, binding ({host, port}) to ({tunnel.local_bind_host, tunnel.local_bind_port})"
+                    )
+                    if ssh_pkey:
+                        os.remove(ssh_pkey)
+                        logging.info("Removed temporary file")
+                    return self.check_mysql_connection(
+                        host=tunnel.local_bind_host,
+                        port=tunnel.local_bind_port,
+                        username=username,
+                        password=password,
+                    )
+            except Exception as e:
+                logging.info(f"Connection failed with exception: {e}")
+                return False
+
+        else:
+            return self.check_mysql_connection(
+                host=host, port=port, username=username, password=password
+            )
