b60631c24a10064b8dd360f6fed4aa758b37a4de
pratyush272 <pratyush272@gmail.com>
2023-07-28 13:07:26 +0530

    Update index.test.tsx

--- frontend/components/Loginscreen/index.test.tsx
+++ frontend/components/Loginscreen/index.test.tsx
@@ -2,15 +2,6 @@ import { render, screen } from '@testing-library/react';
 import Login from './index';
 
 describe('Home', () => {
-  it('renders a login heading', () => {
-    render(<Login />);
-
-    const heading = screen.getByRole('heading', {
-      name: 'Welcome to apperture',
-    });
-
-    expect(heading).toBeInTheDocument();
-  });
 
 
 });
