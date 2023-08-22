72ad6155aacf5e194b385d97aa908c45d2f575bb
anish-parallel <anish@parallelhq.com>
2023-08-22 15:01:31 +0530

    remove unwanted routes

--- frontend/components/AppertureSheets/nossr.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const NoSsr = (props: any) => <>{props.children}</>;
-
-export default dynamic(() => Promise.resolve(NoSsr), {
-  ssr: false,
-});
--- frontend/pages/analytics/appertureSheet.tsx
+++
@@ -1,8 +0,0 @@
-import Sheet from '@components/AppertureSheets/virtual1';
-import React from 'react';
-
-const AppertureSheet = () => {
-  return <Sheet />;
-};
-
-export default AppertureSheet;
