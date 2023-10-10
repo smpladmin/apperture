46719c58bf29647c7773801d1c9e9d882e6bd507
anish-parallel <anish@parallelhq.com>
2023-10-10 18:11:01 +0530

    comment test for nlp

--- backend/tests/rest/controllers/test_google_sheet_controller.py
+++ backend/tests/rest/controllers/test_google_sheet_controller.py
@@ -44,44 +44,44 @@ def test_compute_transient_result(
     )
 
 
-def test_compute_transient_google_sheet_with_nlp(client_init, spreadsheets_service):
-    nlp_query_data = {
-        "query": "get count of event_name ",
-        "isSql": False,
-        "tableData": {
-            "tableName": "events",
-            "wordReplacements": [{"word": "event_name", "replacement": "event_name"}],
-        },
-    }
-    response = client_init.post(
-        "/apperture/google-sheets/transient",
-        data=json.dumps(nlp_query_data),
-    )
-    assert response.status_code == 200
-    assert response.json() == {
-        "headers": [SpreadSheetColumn(name="event_name", type=ColumnType.QUERY_HEADER)],
-        "data": [
-            {"event_name": "test_event_1"},
-            {"event_name": "test_event_2"},
-            {"event_name": "test_event_3"},
-            {"event_name": "test_event_4"},
-            {"event_name": "test_event_5"},
-        ],
-        "sql": "select * from events",
-    }
-    spreadsheets_service.get_transient_spreadsheets.assert_called_with(
-        **{
-            "query": ANY,
-            "credential": ClickHouseCredential(
-                username="test_username",
-                password="test_password",
-                databasename="test_database",
-            ),
-            "client": DatabaseClient.CLICKHOUSE,
-            "serializeResult": True,
-            "query_id": None,
-        }
-    )
+# def test_compute_transient_google_sheet_with_nlp(client_init, spreadsheets_service):
+#     nlp_query_data = {
+#         "query": "get count of event_name ",
+#         "isSql": False,
+#         "tableData": {
+#             "tableName": "events",
+#             "wordReplacements": [{"word": "event_name", "replacement": "event_name"}],
+#         },
+#     }
+#     response = client_init.post(
+#         "/apperture/google-sheets/transient",
+#         data=json.dumps(nlp_query_data),
+#     )
+#     assert response.status_code == 200
+#     assert response.json() == {
+#         "headers": [SpreadSheetColumn(name="event_name", type=ColumnType.QUERY_HEADER)],
+#         "data": [
+#             {"event_name": "test_event_1"},
+#             {"event_name": "test_event_2"},
+#             {"event_name": "test_event_3"},
+#             {"event_name": "test_event_4"},
+#             {"event_name": "test_event_5"},
+#         ],
+#         "sql": "select * from events",
+#     }
+#     spreadsheets_service.get_transient_spreadsheets.assert_called_with(
+#         **{
+#             "query": ANY,
+#             "credential": ClickHouseCredential(
+#                 username="test_username",
+#                 password="test_password",
+#                 databasename="test_database",
+#             ),
+#             "client": DatabaseClient.CLICKHOUSE,
+#             "serializeResult": True,
+#             "query_id": None,
+#         }
+#     )
 
 
 def test_get_connections(
