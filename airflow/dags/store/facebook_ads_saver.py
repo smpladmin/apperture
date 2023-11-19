cf6de11fc3765127acd86ec79301235dc77daba0
anish-parallel <anish@parallelhq.com>
2023-11-19 23:13:14 +0530

    facebook ads dag

---
+++ airflow/dags/store/facebook_ads_saver.py
@@ -0,0 +1,53 @@
+import logging
+from typing import List, Union
+
+from pandas import DataFrame
+
+from domain.datasource.models import ClickHouseRemoteConnectionCred
+from store.clickhouse_client_factory import ClickHouseClientFactory
+
+
+class FacebookAdsDataSaver:
+    def __init__(
+        self,
+        app_id,
+        clickhouse_server_credentials: Union[ClickHouseRemoteConnectionCred, None],
+    ):
+        self.app_id = app_id
+        self.client = ClickHouseClientFactory.get_client(
+            app_id=app_id, clickhouse_server_credentials=clickhouse_server_credentials
+        ).connection
+
+    def convert_header_to_attribute(self, headers: List[str]):
+        attr = ""
+        for header in headers:
+            attr += header + (" datetime ," if header == "date" else " String ,")
+        return attr
+
+    def create_table(
+        self, table_name: str, column_names: List[str], database_name: str
+    ):
+        attributes = self.convert_header_to_attribute(column_names)
+        create_table_query = f"CREATE TABLE IF NOT EXISTS {database_name}.{table_name} ({attributes}) ENGINE = MergeTree ORDER BY date"
+        logging.info(create_table_query)
+        self.client.query(create_table_query)
+
+    def save(
+        self, event_data: DataFrame, table_name: str, database_name: str = "default"
+    ):
+        if not event_data.empty:
+            data = event_data.values.tolist()
+            column_names = event_data.columns.tolist()
+            self.create_table(
+                table_name=table_name,
+                column_names=column_names,
+                database_name=database_name,
+            )
+            logging.info(f"SAVING {len(data)} entries in {database_name}.{table_name}")
+            self.client.insert(
+                data=data,
+                table=table_name,
+                database=database_name,
+                column_names=column_names,
+            )
+        logging.info(f"EVENTS saved successfully!")
