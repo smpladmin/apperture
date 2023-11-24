d0db0d9bf8e456e7e11e4e9322edc0d0db0c4b4d
Aditya Priyam <aditya@parallelhq.com>
2023-11-24 16:37:39 +0530

    airflow CT task concurrency changed to 2

--- airflow/dags/clevertap_data.py
+++ airflow/dags/clevertap_data.py
@@ -75,7 +75,7 @@ def get_run_dates(**kwargs):
     return run_dates
 
 
-@task(max_active_tis_per_dag=5)
+@task(max_active_tis_per_dag=2)
 def load_data(
     datasource: DataSource,
     credential: Credential,
