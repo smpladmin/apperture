6d11de70aedf10b1c38c7a644c17906ed410a9ed
apperture-sahil <sahil@apperture.io>
2024-06-05 14:57:41 +0530

    Compare Queries
    Compare Queries

--- backend/rest/dtos/queries.py
+++ backend/rest/dtos/queries.py
@@ -1,3 +1,5 @@
+from typing import List, Dict
+from enum import Enum
 from pydantic import BaseModel
 from domain.queries.models import Queries
 from rest.dtos.model_response import ModelResponse
@@ -7,7 +9,6 @@ class QueriesTableDto(BaseModel):
     query_id: str
     datasourceId: str
 
-
 class QueriesDto(BaseModel):
     datasourceId: str
     query: str
@@ -16,3 +17,12 @@ class QueriesDto(BaseModel):
 class QueriesResponse(Queries, ModelResponse):
     class Config:
         allow_population_by_field_name = True
+
+
+class QueryComparisonDto(BaseModel):
+    query_ids: List[str]
+    key_columns: List[str]
+    compare_columns: Dict[str, str]
+
+
+
